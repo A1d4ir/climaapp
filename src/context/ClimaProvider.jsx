ca810199817919c9cf948951b38204c5136af5e6
Aldair <aldair_fime@hotmail.com>
2022-04-27 15:07:43 -0500

    Inicio de proyecto de consulta de clima con context api

---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,22 @@
+import { useState, createContext } from "react";
+
+const ClimaContext = createContext()
+
+const ClimaProvider = ({children}) => {
+
+    return (
+        <ClimaContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </ClimaContext.Provider>
+    )
+}
+
+export {
+    ClimaProvider
+}
+
+export default ClimaContext
